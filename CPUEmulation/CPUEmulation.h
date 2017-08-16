14b00cf46e48b1bfadcda813afa9d91446cacd04
Will Stafford <wrsford@Wills-MacBook-Pro.local>
2017-08-16 15:07:23 -0400

    Initial Commit

---
+++ CPUEmulation/CPUEmulation.h
@@ -0,0 +1,19 @@
+//
+//  CPUEmulation.h
+//  CPUEmulation
+//
+//  Created by Will Stafford on 8/16/17.
+//  Copyright © 2017 Wrsford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CPUEmulation.
+FOUNDATION_EXPORT double CPUEmulationVersionNumber;
+
+//! Project version string for CPUEmulation.
+FOUNDATION_EXPORT const unsigned char CPUEmulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CPUEmulation/PublicHeader.h>
+
+
---
+++ CPUEmulationTests/CPUEmulationTests.swift
@@ -0,0 +1,36 @@
+//
+//  CPUEmulationTests.swift
+//  CPUEmulationTests
+//
+//  Created by Will Stafford on 8/16/17.
+//  Copyright © 2017 Wrsford. All rights reserved.
+//
+
+import XCTest
+@testable import CPUEmulation
+
+class CPUEmulationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
